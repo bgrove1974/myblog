194a0117969ddb9b553c5623211384622d1599ad
bgroveben <bgroveben@gmail.com>
2019-09-16 02:37:45 -0700

    Write a method to keep score during the blackjack game

--- blackjack.py
+++ blackjack.py
@@ -22,3 +22,19 @@ card_value = {
     'Q': 10,
     'K': 10
 }
+
+def calculate_score(card_list):
+    """
+    Calculates the sum of the cards and compensates for the Ace.
+    card_list: list of cards
+    Returns:
+        total: score of the list of cards.
+    """
+    total = 0
+    for card in card_list:
+        # Ace can be either 1 or 11:
+        if total >= 11 and card == 'A':
+            total += 1
+        else:
+            total += card_value[card]
+    return total
