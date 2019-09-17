ac8539725eacd7e34b0085218e441cbc044ad652
bgroveben <bgroveben@gmail.com>
2019-09-17 02:41:14 -0700

    Write a method for one round of blackjack between dealer and player

--- blackjack.py
+++ blackjack.py
@@ -38,3 +38,33 @@ def calculate_score(card_list):
         else:
             total += card_value[card]
     return total
+
+
+def play_turn(deck):
+    """
+    One round of blackjack with player and dealer each taking a turn.
+    deck: list of available cards
+    Returns:
+        Total score for the turn
+    """
+    # Store the cards that are dealt in a list:
+    table_cards = []
+    user_input = 'y'
+    # Deal cards while user chooses 'y' and hasn't busted:
+    while user_input == 'y':
+        # Calculate a random position for the card:
+        position = int(random() * len(deck))
+        # Deal the card:
+        print(deck[position])
+        table_cards.append(deck[position][0])
+        # Make sure you don't keep dealing the same card:
+        del deck[position]
+        # If player busts (goes over 21), break out of the loop:
+        if calculate_score(table_cards) >= 21:
+            break
+        # Ask the user if they want another card:
+        user_input = raw_input("Do you want another card? (enter 'y') ")
+    # Print player's score:
+    print "Your score is %d" % calculate_score(table_cards)
+    # Return the total score for the round:
+    return calcualte_score(table_cards)
