763aad9c5d1ffd55ea9af2748930d668dd8a9a9a
bgroveben <bgroveben@gmail.com>
2019-10-30 23:13:53 -0700

    Write a method to determine valid moves in Othello game

--- reversegam.py
+++ reversegam.py
@@ -26,3 +26,48 @@ def getNewBoard():
     """
     Create a brand-new blank board data structure.
     """
+    board = []
+    for i in range(WIDTH):
+        board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+    return board
+
+def isValidMove(board, tile, xstart, ystart):
+    """
+    Return False if the player's move on xstart, ystart is invalid.
+    Otherwise, return a list of spaces that the player would take over.
+    """
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+        return False
+
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'
+
+    tilesToFlip = []
+    for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1],[0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+        x, y = xstart, ystart
+        # First step in the x direction:
+        x += xdirection
+        # First step in the y direction:
+        y += ydirection
+        while isOnBoard(x, y) and board[x][y] == otherTile:
+            # Keep moving in this x & y direction:
+            x += xdirection
+            y += ydirection
+            if isOnBoard(x, y) and board[x][y] == tile:
+            # There are pieces to flip over.
+            # Go in the reverse direction until we reach the original space.
+            while True:
+                x -= xdirection
+                y -= ydirection
+                if x == xstart and y == ystart:
+                    break
+                tilesToFlip.append([x, y])
+    if len(tilesToFlip) == 0:
+        # If no tiles were flipped, then this move is invalid.
+        return False
+    return tilesToFlip
+
+def isOnBoard(x, y):
+    pass
